841b7553d288ae681152d859165976db7b344588
Студенческая <@rbiu.local>
2025-11-21 13:04:09 +0500

    shop

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './style.css'
+import 'flowbite';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';

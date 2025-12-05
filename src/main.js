1e74107825725f412ef3b3e67463f0455132851f
Студенческая <@rbiu.local>
2025-12-05 13:02:48 +0500

    vite-shop

--- src/main.js
+++ src/main.js
@@ -6,3 +6,13 @@ import { Navigation, Pagination} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+<script>
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  </script>

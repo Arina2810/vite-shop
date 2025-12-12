27da4452373e764e69103cb91ea96b17f7f5e023
Студенческая <@rbiu.local>
2025-12-12 13:09:15 +0500

    shop

--- src/main.js
+++ src/main.js
@@ -2,17 +2,19 @@ import './style.css'
 import 'flowbite';
 
 import Swiper from 'swiper';
-import { Navigation, Pagination} from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-<script>
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 2,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-  </script>
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+function resetFilters() {
+  document.querySelectorAll('.filters input[type="checkbox"]').forEach(cb => cb.checked = false);
+}
